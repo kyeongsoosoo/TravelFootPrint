0ee1192ebbda063090e42d2b037fadf70e9cee5d
kyeongsoosoo <shakramon@naver.com>
2021-02-26 06:02:29 +0900

    7차수정
    백엔드 배포 테스트

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node backends/index.js
--- backends/index.js
+++ backends/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
