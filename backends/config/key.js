bc779d6e866f3c7363ee12c9d4a04f5e38fc3161
kyeongsoosoo <shakramon@naver.com>
2021-03-03 08:17:28 +0900

    add key

---
+++ backends/config/key.js
@@ -0,0 +1,11 @@
+const env = require('dotenv');
+
+env.config();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+module.exports = {
+  CLIENT_ID,
+  CLIENT_SECRET,
+};
